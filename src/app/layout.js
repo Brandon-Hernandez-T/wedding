c23445ce3eaf85c226128609a2223a3489191dbd
randon-Hernandez-T <braniitorres@gmail.com>
2024-03-19 00:12:19 -0600

    Update Favicon and metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,14 +7,21 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "B&J | Nuestra Boda",
-  description: "Nos casamos!! B&J",
+  description: "Nos casamos!! Jacqueline & Brandon ",
   image: "/novios-icono.png"
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <link rel="icon" href="/favicon.ico" />
+      {/* <link rel="icon" href="/favicon.ico" /> */}
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:image" content={metadata.image} />
+        <meta property="og:type" content="website" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
